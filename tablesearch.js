b774ed9d6c25a133d76e1174438ce4eaea8f33be
Dominick Lee <dnicky2288@yahoo.com>
2018-11-14 18:37:45 -0500

    Fixed 2nd category search

--- tablesearch.js
+++ tablesearch.js
@@ -2,7 +2,7 @@ function searchTable(input, tableId, searchColumn = -1, searchColumn2 = -1)
 {
     var filter = input.toUpperCase();
 	var table = document.getElementById(tableId);
-	var rowText;	//temporary var
+	var rowText, rowText2;	//temporary var
 	
 	for (var i = 0, row; row = table.rows[i]; i++) {
 		//iterate through rows
@@ -21,22 +21,27 @@ function searchTable(input, tableId, searchColumn = -1, searchColumn2 = -1)
 		else
 		{
 			if (i != 0)	{	//don't perform search on the first row header
-				rowText = row.cells[searchColumn].innerHTML;	//get text for the whole row
-				if (rowText.toUpperCase().indexOf(filter) > -1) {	//if upper case matches any part of filter
-					row.style.display = "";
-				}
-				else {
-					row.style.display = "none";
-				}
 				
-				if (searchColumn2 != -1) {	//and also search in 2nd category too
-					rowText = row.cells[searchColumn2].innerHTML;	//get text for the whole row
-					if (rowText.toUpperCase().indexOf(filter) > -1) {	//if upper case matches any part of filter
+				if (searchColumn2 != -1) {	//2 category search
+					rowText = row.cells[searchColumn].innerHTML;	//get text for the 1st column
+					rowText2 = row.cells[searchColumn2].innerHTML;	//get text for the 2nd column
+					if (rowText.toUpperCase().indexOf(filter) > -1 || rowText2.toUpperCase().indexOf(filter) > -1) {	//if upper case matches any part of filter
 						row.style.display = "";
 					}
 					else {
 						row.style.display = "none";
 					}	
+					
+				}
+				else {	//single category search
+					rowText = row.cells[searchColumn].innerHTML;	//get text for the whole row
+					if (rowText.toUpperCase().indexOf(filter) > -1) {	//if upper case matches any part of filter
+						row.style.display = "";
+					}
+					else {
+						row.style.display = "none";
+					}
+					
 				}
 			}
 		}
