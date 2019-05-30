55ffe87be20ac077b7ace337410d7bb4b3767056
Dominick Lee <dnicky2288@yahoo.com>
2019-05-30 06:55:30 -0400

    Improved tablesearch. Added tablesort

--- README.md
+++ README.md
@@ -18,3 +18,9 @@ Refer to the `index.html` page for examples. To use this JS function, you simply
 For example, if you want to search for last names, which are in column 2 (counting from 0), you can search for items in a specific column like this: `onkeyup="searchTable(this.value, 'list', 2)"`
 
 If you want to search in two specific columns (e.g. first and last name) which are respectively in columns 1 and 2, then you can use more specific arguments: `onkeyup="searchTable(this.value, 'list', 1, 2)"`
+
+Likewise, if you want to search in three specific columns (e.g. first, middle, and last name) which are respectively in columns 1, 2, and 3, then you can use this argument: `onkeyup="searchTable(this.value, 'list', 1, 2, 3)"`
+
+## Sorting table rows numerically
+
+An added feature is the `tablesort.js` which has been added. This will allow you to sort through a table on the column of index 0. Obviously, the column index can be changed as well as the table name as needed. Use an argument like: `sortTable("myTable", 0)`
--- tablesearch.js
+++ tablesearch.js
@@ -1,4 +1,4 @@
-function searchTable(input, tableId, searchColumn = -1, searchColumn2 = -1) 
+function searchTable(input, tableId, searchColumn = -1, searchColumn2 = -1, searchColumn3 = -1) 
 {
     var filter = input.toUpperCase();
 	var table = document.getElementById(tableId);
@@ -21,8 +21,20 @@ function searchTable(input, tableId, searchColumn = -1, searchColumn2 = -1)
 		else
 		{
 			if (i != 0)	{	//don't perform search on the first row header
-				
-				if (searchColumn2 != -1) {	//2 category search
+
+				if (searchColumn3 != -1) {	//3 category search
+					rowText = row.cells[searchColumn].innerHTML;	//get text for the 1st column
+					rowText2 = row.cells[searchColumn2].innerHTML;	//get text for the 2nd column
+					rowText3 = row.cells[searchColumn3].innerHTML;	//get text for the 3rd column
+					if (rowText.toUpperCase().indexOf(filter) > -1 || rowText2.toUpperCase().indexOf(filter) > -1 || rowText3.toUpperCase().indexOf(filter) > -1) {	//if upper case matches any part of filter
+						row.style.display = "";
+					}
+					else {
+						row.style.display = "none";
+					}	
+					
+				}			
+				else if (searchColumn2 != -1) {	//2 category search
 					rowText = row.cells[searchColumn].innerHTML;	//get text for the 1st column
 					rowText2 = row.cells[searchColumn2].innerHTML;	//get text for the 2nd column
 					if (rowText.toUpperCase().indexOf(filter) > -1 || rowText2.toUpperCase().indexOf(filter) > -1) {	//if upper case matches any part of filter
